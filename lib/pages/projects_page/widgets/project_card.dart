2f009c1275314c258430aa5c40feaa8c8f22a1e3
tecrodrigocastro <tec.rodrigocastro@gmail.com>
2023-04-15 15:23:36 -0300

    projec card model

--- lib/pages/projects_page/widgets/project_card.dart
+++ lib/pages/projects_page/widgets/project_card.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:redrodrigo/pages/projects_page/entity/project_entity.dart';
 import 'package:redrodrigo/shared/constants/colors_schema.dart';
+import 'package:redrodrigo/shared/constants/constants.dart';
+import 'package:url_launcher/url_launcher_string.dart';
 
 class ProjectCard extends StatefulWidget {
   const ProjectCard({super.key, required this.project});
@@ -115,6 +117,77 @@ class _ProjectCardState extends State<ProjectCard> {
                 ),
               ),
             ),
+            if (widget.project.githubLink != null ||
+                widget.project.liveProjectLink != null)
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    const Spacer(),
+                    if (widget.project.liveProjectLink != null) ...[
+                      DecoratedBox(
+                        decoration: BoxDecoration(
+                          border: Border.all(
+                            color: secondaryWhiteColor,
+                          ),
+                          borderRadius: BorderRadius.circular(5),
+                        ),
+                        child: TextButton(
+                          onPressed: () async {
+                            await launchUrlString(
+                                widget.project.liveProjectLink!);
+                          },
+                          child: const Text(
+                            'live-project',
+                            style: TextStyle(
+                              color: secondaryWhiteColor,
+                            ),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(
+                        width: 8,
+                      )
+                    ],
+                    if (widget.project.githubLink != null)
+                      DecoratedBox(
+                        decoration: BoxDecoration(
+                          border: Border.all(
+                            color: secondaryWhiteColor,
+                          ),
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        child: TextButton(
+                          style: TextButton.styleFrom(
+                              backgroundColor: primaryColorDark),
+                          onPressed: () async {
+                            await launchUrlString(widget.project.githubLink!);
+                          },
+                          child: Row(
+                            children: [
+                              const Text(
+                                'view-code',
+                                style: TextStyle(
+                                  color: secondaryWhiteColor,
+                                ),
+                              ),
+                              const SizedBox(
+                                width: 2,
+                              ),
+                              Image.asset(
+                                githubIconPath,
+                                width: 18,
+                                height: 18,
+                                color: secondaryWhiteColor,
+                              ),
+                            ],
+                          ),
+                        ),
+                      )
+                  ],
+                ),
+              ),
           ],
         ),
       ),
--- lib/shared/constants/constants.dart
+++ lib/shared/constants/constants.dart
@@ -1,2 +1,6 @@
 const kFontFamily = 'FiraCode';
 const aboutMePageRoute = 'aboutme';
+const String twitterIconPath = 'assets/icons/twitter.png';
+const String linkedinIconPath = 'assets/icons/linkedin.png';
+const String githubIconPath = 'assets/icons/github.png';
+const String instagramIconPath = 'assets/icons/instagram.png';
